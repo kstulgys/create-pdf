2febbeafd3ff5d81a482f17d22bf30859e7e7989
kstulgys <karolis.stulgys@gmail.com>
2022-03-13 20:57:27 +0200

    wip

--- server.js
+++ server.js
@@ -1,7 +1,10 @@
 const express = require("express");
 const chromium = require("chrome-aws-lambda");
+var cors = require("cors");
 
 const app = express();
+app.use(cors());
+
 const PORT = process.env.PORT || 3001;
 
 async function generatePDF({ html = "", margin }) {
