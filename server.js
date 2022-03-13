2414eed7c3c216063692da5d0a53ad5b564f8f0f
kstulgys <karolis.stulgys@gmail.com>
2022-03-14 00:04:46 +0200

    wip

--- server.js
+++ server.js
@@ -11,8 +11,7 @@ const PORT = process.env.PORT || 3001;
 async function generatePDF({ html = "", margin }) {
   const browser = await puppeteer.launch({
     // args: chromium.args,
-    // args: ["--no-sandbox", "--disable-setuid-sandbox"],
-    args: ["--no-sandbox", "--font-render-hinting=none"],
+    args: ["--no-sandbox"],
     // defaultViewport: chromium.defaultViewport,
     // executablePath: await chromium.executablePath,
     ignoreHTTPSErrors: true,
