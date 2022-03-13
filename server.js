b93cc6e52d5a2802504307313d8708c58bca90cd
kstulgys <karolis.stulgys@gmail.com>
2022-03-13 23:20:43 +0200

    wip

--- server.js
+++ server.js
@@ -20,7 +20,7 @@ async function generatePDF({ html = "", margin }) {
   });
   const page = await browser.newPage();
 
-  await page.setContent(html);
+  await page.setContent(html, { waitUntil: "domcontentloaded" });
   await page.emulateMediaType("print");
   const { bottom = "1cm", top = "1cm", left = "3cm", right = "1.5cm" } = margin;
 
