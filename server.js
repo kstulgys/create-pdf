4dcc057613700b121be313b3c6262d4009f9ef96
kstulgys <karolis.stulgys@gmail.com>
2022-03-13 21:01:53 +0200

    wip

--- server.js
+++ server.js
@@ -3,7 +3,16 @@ const chromium = require("chrome-aws-lambda");
 var cors = require("cors");
 
 const app = express();
-app.use(cors());
+// app.use(cors());
+
+app.use(function (req, res, next) {
+  res.header("Access-Control-Allow-Origin", "*"); // update to match the domain you will make the request from
+  res.header(
+    "Access-Control-Allow-Headers",
+    "Origin, X-Requested-With, Content-Type, Accept"
+  );
+  next();
+});
 
 const PORT = process.env.PORT || 3001;
 
