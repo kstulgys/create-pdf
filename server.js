ceb551a4726a109569f7a6b4b78627b250507d25
kstulgys <karolis.stulgys@gmail.com>
2022-03-13 20:49:13 +0200

    wip

---
+++ server.js
@@ -0,0 +1,57 @@
+const express = require("express");
+const chromium = require("chrome-aws-lambda");
+
+const app = express();
+const PORT = process.env.PORT || 3001;
+
+async function generatePDF({ html = "", margin }) {
+  const browser = await chromium.puppeteer.launch({
+    args: chromium.args,
+    defaultViewport: chromium.defaultViewport,
+    executablePath: await chromium.executablePath,
+    ignoreHTTPSErrors: true,
+    headless: true,
+  });
+  const page = await browser.newPage();
+
+  await page.setContent(html);
+  await page.emulateMediaType("print");
+  const { bottom = "1cm", top = "1cm", left = "3cm", right = "1.5cm" } = margin;
+
+  const pdfBuffer = await page.pdf({
+    format: "a4",
+    printBackground: true,
+    margin: { bottom, top, left, right },
+  });
+
+  await page.close();
+  await browser.close();
+
+  return pdfBuffer;
+}
+
+app.post("/create-pdf", async (req, res) => {
+  const { styleTags = "", innerHTML = "", margin = {} } = req.body;
+  console.log({ innerHTML });
+  const html = `<html>
+    <head>
+      <meta charset="UTF-8">
+      <meta name="viewport" content="width=device-width, initial-scale=1.0">
+      <style>
+        ${styleTags}
+      </style>
+    </head>
+    <body>
+      ${innerHTML}
+    </body>
+  </html>
+  `;
+  const pdf = await generatePDF({ html, margin });
+  res.setHeader("Content-Type", "application/pdf");
+  res.send(pdf);
+});
+
+app.listen(PORT, (err) => {
+  if (err) throw err;
+  console.info(`App listening on port ${PORT}`);
+});
