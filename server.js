4f7d48ba57ae7362ea82c33e41e4f69cdd658eb8
kstulgys <karolis.stulgys@gmail.com>
2022-03-13 22:22:08 +0200

    wip

--- server.js
+++ server.js
@@ -36,30 +36,6 @@ async function generatePDF({ html = "", margin }) {
   return pdfBuffer;
 }
 
-// app.use(function (req, res, next) {
-//   // Website you wish to allow to connect
-//   res.setHeader("Access-Control-Allow-Origin", "http://localhost:3000");
-
-//   // Request methods you wish to allow
-//   res.setHeader(
-//     "Access-Control-Allow-Methods",
-//     "GET, POST, OPTIONS, PUT, PATCH, DELETE"
-//   );
-
-//   // Request headers you wish to allow
-//   res.setHeader(
-//     "Access-Control-Allow-Headers",
-//     "X-Requested-With,content-type"
-//   );
-
-//   // Set to true if you need the website to include cookies in the requests sent
-//   // to the API (e.g. in case you use sessions)
-//   res.setHeader("Access-Control-Allow-Credentials", true);
-
-//   // Pass to next layer of middleware
-//   next();
-// });
-
 app.use(cors());
 app.use(bodyParser.json({ limit: "10mb" }));
 
@@ -69,7 +45,7 @@ app.get("/health", (req, res) => {
 
 app.post("/create-pdf", async (req, res) => {
   const { styleTags = "", innerHTML = "", margin = {} } = req.body;
-  console.log({ innerHTML });
+
   const html = `<html>
     <head>
       <meta charset="UTF-8">
@@ -85,7 +61,7 @@ app.post("/create-pdf", async (req, res) => {
   `;
   const pdf = await generatePDF({ html, margin });
   res.setHeader("Content-Type", "application/pdf");
-  res.send(pdf);
+  res.status(200).send(pdf);
 });
 
 app.listen(PORT, (err) => {
